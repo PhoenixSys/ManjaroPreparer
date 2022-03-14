f9b41e7b05041c769c8554da8e6cc1abaaa0416b
Yazdan <mrrezayazdani@yahoo.com>
2022-03-14 17:11:34 +0330

    Set quiet mode for install apps and configured zsh-autocompletd plugin

--- installer.sh
+++ installer.sh
@@ -1,33 +1,33 @@
 #! /bin/bash
 
 # Update repository:
-sudo pacman -Syu
+sudo pacman -Sqyu
 
 # Install aur package manager
-sudo pacman -S yay pamac
+sudo pacman -Sq yay pamac
 
 # Base system applications
-sudo pacman -S git screenfetch zsh gparted net-tools copyq htop appimagelauncher
-yay -S nerdfetch pantheon-terminal translate-shell http gitflow stacer
+sudo pacman -Sq git screenfetch zsh gparted net-tools copyq htop appimagelauncher
+yay -Sq nerdfetch pantheon-terminal translate-Sqhell http gitflow stacer
 
 # Programming language
 
 ## python
-sudo pacman -S python ipython python-pip virtualbox
+sudo pacman -Sq python ipython python-pip virtualbox
 
 ### base python modules
 sudo pip install -U pip
 sudo pip install -U django flask sanic gunicorn pyfiglet shecan
 
 ## node.js
-sudo pacman -S nodejs npm
+sudo pacman -Sq nodejs npm
 
 # ClamAV antivirus
-sudo pacman -S clamav clamtk
+sudo pacman -Sq clamav clamtk
 
 # Graphic tools
-sudo pacman -S krita gimp
-yay -S lorien-bin rnote
+sudo pacman -Sq krita gimp
+yay -Sq lorien-bin rnote
 
 # Shecan dns :
 sudo shecan update
@@ -36,51 +36,54 @@ sudo shecan set
 sudo shecan verify
 
 # Sochial applications
-sudo pacman -S telegram-desktop discord
+sudo pacman -Sq telegram-desktop discord
 
 # Database
 
 ## postgresql
-sudo pacman -S postgresql pgadmin4
+sudo pacman -Sq postgresql pgadmin4
 sudo -u postgres -b 'initdb --locale $LANG -E UTF8 -D "/var/lib/postgres/data/"'
 sudo systemctl start postgresql.service
 sudo systemctl enable postgresql.service
 
 ## mongodb
-yay -S mongodb-bin mongodb-compass
+yay -Sq mongodb-bin mongodb-compass
 sudo systemctl start mongodb.service
 sudo systemctl enable mongodb.service
 
 ## redis
-sudo pacman -S redis
+sudo pacman -Sq redis
 sudo systemctl start redis
 sudo systemctl enable redis
 
 ## sqlite
-sudo pacman -S sqlite
+sudo pacman -Sq sqlite
 
 # Browsers
-sudo pacman -S firefox links torbrowser-launcher
-yay -S google-chrome
+sudo pacman -Sq firefox links torbrowser-launcher
+yay -Sq google-chrome
 sudo pamac install brave-browser
 
 # Office softwares
-sudo pacman -S libreoffice-fresh
+sudo pacman -Sq libreoffice-fresh
 
 # Multimedia 
-sudo pacman -S clementine vlc elisa simplescreenrecorder
+sudo pacman -Sq clementine vlc elisa simplescreenrecorder
 
 # Development IDE
-sudo pacman -S kate code
+sudo pacman -Sq kate code
 
 # Virtualization
-sudo pacman -S docker docker-compose
+sudo pacman -Sq docker docker-compose
 sudo systemctl start docker
 sudo systemctl enable docker
 
 # Server tools:
-sudo pacman -S remmina
-yay -S wrk
+sudo pacman -Sq remmina
+yay -Sq wrk
+
+# Api client
+yay -Sq insomnia
 
 # Warp VPN
-yay -S cloudflare-warp-bin
+yay -Sq cloudflare-warp-bin
--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -3,12 +3,16 @@
 # install ohmyzsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# clone zsh-autocomplete
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+
+
 # install linear theme
 wget https://raw.githubusercontent.com/MrYazdan/zsh-linear-theme/main/linear.zsh-theme -O ~/.oh-my-zsh/themes/linear.zsh-theme
-omz theme set linear 
 
 # set startup
 echo "nerdfetch \necho ''" > ~/.zshenv
 
 # add plugins
 omz plugin enable git python colorize git-flow docker zsh-autosuggestions
+omz theme set linear 
