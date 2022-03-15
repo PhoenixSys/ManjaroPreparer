bc716123e79e27526f5880e44d9145f41296b0b9
Reza Yazdani <74140299+MrYazdan@users.noreply.github.com>
2022-03-15 16:30:43 +0330

    Update ohmyzsh.sh

--- ohmyzsh.sh
+++ ohmyzsh.sh
@@ -6,13 +6,11 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # clone zsh-autocomplete
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 
-
-# install linear theme
-wget https://raw.githubusercontent.com/MrYazdan/zsh-linear-theme/main/linear.zsh-theme -O ~/.oh-my-zsh/themes/linear.zsh-theme
+# install and set linear theme
+wget https://raw.githubusercontent.com/MrYazdan/zsh-linear-theme/main/linear.zsh-theme -O ~/.oh-my-zsh/themes/linear.zsh-theme && omz theme set linear
 
 # set startup
 echo "nerdfetch \necho ''" > ~/.zshenv
 
 # add plugins
-omz plugin enable git python colorize git-flow docker zsh-autosuggestions
-omz theme set linear 
+omz plugin enable git python colorize git-flow docker zsh-autosuggestions 
